bf0d010547bd8664864487a1a8f32d3d12e98e71
tunglamlqddb <tunglamlqddb@gmail.com>
2022-03-14 19:03:26 +0700

    fix bug

--- src/main_incremental.py
+++ src/main_incremental.py
@@ -235,6 +235,7 @@ def main(argv=None):
             appr.model = torch.load(os.path.join(os.path.join(args.results_path, full_exp_name), "models", "test_model.pt"))
             appr.model.to(device)
             train = False
+            appr.post_train_process()
             
         print('*' * 108)
         print('Task {:2d}'.format(t))
